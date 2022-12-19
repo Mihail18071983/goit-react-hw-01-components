fa897bee1cb38b1ef00f1aef6c4d7e72027f89dd
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2022-12-19 19:17:09 +0200

    profile

---
+++ src/components/Profile/App.jsx
@@ -0,0 +1,16 @@
+import user from './user.json';
+import { Profile } from './Profile';
+export const App = () => {
+  return (
+    <>
+      <Profile
+        key={user.username}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
+  );
+};
