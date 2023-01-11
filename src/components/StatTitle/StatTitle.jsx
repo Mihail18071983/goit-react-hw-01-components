82715d01f6d0ec82e6fcb6594c043ec2ed36076f
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-01-11 11:35:12 +0200

    Update StatTitle.jsx

--- src/components/StatTitle/StatTitle.jsx
+++ src/components/StatTitle/StatTitle.jsx
@@ -1,9 +1,9 @@
 import css from './StatTitle.module.css';
 import PropTypes from 'prop-types';
 
-export const StatTitle = ({ title }) => {
+export const StatTitle = ({ title }) => (
   <h2 className={css.title}>{title}</h2>
-};
+);
 
 StatTitle.propTypes = {
   title: PropTypes.string,
